09abcfb47e924047ee49d98a74debbf35dc24ed7
Khanh Chung <justinngo1997@gmail.com>
2021-10-14 11:40:54 -0700

    add docstring to multiply_list function

--- multiply.py
+++ multiply.py
@@ -1,4 +1,29 @@
 def multiply_list(nums):
+    """
+    Multiply elements in a list.
+
+    Parameters
+    ----------
+    nums : list of int
+        elements as input in a list.
+
+    Returns
+    -------
+    int:
+        The product of all elements.
+    
+    False:
+        If any element in the list is invalid.
+
+    Examples
+    --------
+    >>> multiply_list([])
+    False
+    >>> multiply_list([1, 2, 3, 4, 5])
+    120
+    >>> multiply_list([1, 2, 'a', 4, 5])
+    False
+    """
     product = 1
     for num in nums:
         if type(num) != int:
