5523c8feb89dd16af6c69075c234a1910ed2aeb3
Khanh Chung <justinngo1997@gmail.com>
2021-10-14 12:53:18 -0700

    add docstring for calculate_time function

--- timing.py
+++ timing.py
@@ -1,6 +1,19 @@
 import time
 
 def calculate_time(func):
+    """
+    Calculate and print the time to run a function in seconds.
+
+    Parameters
+    ----------
+    func : function
+        The function to run
+
+    Examples
+    --------
+    >>> calculate_time(func)
+    2.0001738937
+    """
     current = time.time()
     func()
     end = time.time()
